6da0aef595d309d65f900b04c76327e3939c0cec
sanaos <96974633+sanaos@users.noreply.github.com>
2024-10-23 08:49:58 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -474,6 +474,7 @@ export default {
           "sub.cm": "https://sub.cm/short",
         },
         customBackend: {
+          "老铁":"https://p01--subweb--mspfljbftfzq.code.run/sub?"
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
           "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
