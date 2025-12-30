429e059dd3c85f789b669df928d6710cbd30e32b
sanaos <96974633+sanaos@users.noreply.github.com>
2025-12-30 15:15:33 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -475,8 +475,7 @@ export default {
         },
         customBackend: {
           "【老铁】": "https://p01--subweb--mspfljbftfzq.code.run",
-          "【老铁2号】": "https://zjpcvjxpewku.ap-northeast-1.clawcloudrun.com",
-          "【老铁3号】": "https://tpmcjpytayje.ap-southeast-1.clawcloudrun.com",
+          "【老铁2号】": "https://phxemflhmhcd.ap-northeast-1.clawcloudrun.com",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
           "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
@@ -1428,6 +1427,7 @@ export default {
   }
 };
 </script>
+
 
 
 
